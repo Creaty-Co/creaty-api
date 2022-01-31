f739c8720f48c7406480a370bab79d9a9ced4864
GentleEnvy <komarov.sergei163@gmail.com>
2022-01-31 18:09:52 +0300

    add Category and Tag title translatable (+ migration) fix #14

--- api/translation.py
+++ api/translation.py
@@ -1,1 +1,13 @@
 from modeltranslation.translator import register, TranslationOptions
+
+from tags.models import *
+
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ['title']
+
+
+@register(Tag)
+class TagTranslationOptions(TranslationOptions):
+    fields = ['title']
