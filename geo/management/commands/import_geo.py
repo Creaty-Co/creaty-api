952a921711cec98291e36eea6c8d9c151fa36ac0
GentleEnvy <komarov.sergei163@gmail.com>
2022-02-01 15:50:43 +0300

    add import_geo command fix #29

--- geo/management/commands/import_geo.py
+++ geo/management/commands/import_geo.py
@@ -1,8 +1,26 @@
+import json
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils import translation
+
+from django_countries import countries as raw_countries
 
 from geo.models import *
 
 
 class Command(BaseCommand):
+    FLAG_UNICODES_FILEPATH = 'geo/management/commands/flag_unicodes.json'
+    
     def handle(self, *args, **options):
-        pass
+        flag_unicodes = json.load(open(self.FLAG_UNICODES_FILEPATH))
+        countries = {country.code: country for country in Country.objects.all()}
+        for language, _ in settings.LANGUAGES:
+            translation.activate(language)
+            for code, name in dict(raw_countries).items():
+                code = code.lower()
+                country = countries.setdefault(code, Country(code=code))
+                setattr(country, f'name_{language}', name)
+                country.flag_unicode = flag_unicodes[code]
+        for country in countries.values():
+            country.save()
