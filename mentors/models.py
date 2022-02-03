dac96cee41ba44478e6093a597ba26b5fbea4928
GentleEnvy <komarov.sergei163@gmail.com>
2022-02-03 21:29:26 +0300

    add form models migration

--- mentors/models.py
+++ mentors/models.py
@@ -1,4 +1,5 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 from djmoney.models.fields import MoneyField
 
 from base.models import AbstractModel
