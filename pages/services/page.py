7fd0cb2e831b84d888a89c90aaa71759bf370b08
GentleEnvy <komarov.sergei163@gmail.com>
2022-02-06 22:52:41 +0300

    add PageService

---
+++ pages/services/page.py
@@ -0,0 +1,30 @@
+from mentors.models import Mentor
+from pages.models import Page, PageMentorSet, PageTagSet
+from tags.models import Category, Tag
+
+
+class PageService:
+    MAX_MENTORS_COUNT = 20
+    MAX_TAGS_COUNT = 20
+    
+    def get_or_create(self, tag_or_category: Tag | Category) -> Page:
+        if isinstance(tag_or_category, Tag):
+            page, is_created = Page.objects.get_or_create(tag=tag_or_category)
+        else:
+            page, is_created = Page.objects.get_or_create(category=tag_or_category)
+        if is_created:
+            self._fill_random(page)
+        return page
+    
+    def _fill_random(self, page: Page) -> None:
+        mentor_qs = Mentor.objects.order_by('?')[:self.MAX_MENTORS_COUNT]
+        tags_qs = Tag.objects.order_by('?')[:self.MAX_TAGS_COUNT]
+        if page.tag is None:
+            mentor_qs = mentor_qs.filter(tag_set__category=page.category)
+        else:
+            mentor_qs = mentor_qs.filter(tag_set=page.tag)
+            tags_qs = tags_qs.exclude(id=page.tag.id)
+        for index, mentor in enumerate(mentor_qs):
+            PageMentorSet.objects.create(page=page, mentor=mentor, index=index)
+        for index, tag in enumerate(tags_qs):
+            PageTagSet.objects.create(page=page, tag=tag, index=index)
