a2a3a21b86c9fc80bfa52d6a76feb49fd216b755
GentleEnvy <komarov.sergei163@gmail.com>
2022-02-08 22:41:54 +0300

    fix geo urls

--- geo/urls.py
+++ geo/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('languages/', GeoLanguagesView.as_view()),
-    path('countries/',  .as_view()),
+    path('countries/', GeoCountriesView.as_view()),
 ]
