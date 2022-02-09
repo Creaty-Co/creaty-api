6aba7de648ed6fd828da4b2bbc8eaafb241ce900
GentleEnvy <komarov.sergei163@gmail.com>
2022-02-09 18:41:26 +0300

    update MentorsFilterSet

--- mentors/filters/general.py
+++ mentors/filters/general.py
@@ -3,6 +3,15 @@ from base.filters.base import *
 
 
 class MentorsFilterSet(BaseFilterSet):
+    tag_set__in = ArrayFilter(
+        base_field=forms.IntegerField(), method='filter_tag_set__in',
+        label='Tag set в'
+    )
+    
     class Meta:
         model = Mentor
-        fields = {'tag_set': ['exact']}
+        fields = {}
+    
+    @staticmethod
+    def filter_tag_set__in(queryset, name, value):
+        return queryset.filter(**{name: value}).distinct()
