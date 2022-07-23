dc614792953d7739c91fe3a7e914a17b46481b0c
GentleEnvy <komarov.sergei163@gmail.com>
2022-07-23 15:48:54 +0300

    fix #78

--- forms/services/applications_xlsx.py
+++ forms/services/applications_xlsx.py
@@ -1,8 +1,11 @@
+import re
+
 from django.db.models import F
 
 from base.services.xlsx import BaseXlsxConverter
 from forms.models import Application, Form
 from forms.models.choices import FormField, FormType, rFormType
+from mentors.models import Mentor
 
 _FormTypeByLabel = {_.label: _ for _ in FormType}
 
@@ -11,7 +14,7 @@ class ApplicationsXlsxConverter(BaseXlsxConverter):
     MODEL = Application
     FIELD_HEADER_MAP = {'type': 'Тип', 'id': 'Id', 'path': 'Path'} | {
         _.value: _.label for _ in FormField
-    }
+    } | {'mentor_fullname': 'Имя ментора'}
     
     def __init__(self):
         super().__init__(self.FIELD_HEADER_MAP)
@@ -20,10 +23,15 @@ class ApplicationsXlsxConverter(BaseXlsxConverter):
         values = list(
             self.MODEL.objects.annotate(type=F('form__type')).order_by(
                 'type', 'id'
-            ).values_list(*self.fields)
+            ).values_list(*self.fields[:-1])
         )
         for i, value in enumerate(values):
-            values[i] = (rFormType[value[0]].label, *value[1:])
+            application_type = rFormType[value[0]]
+            mentor_fullname = ''
+            if match := re.match(r'/user/(\d+).*', value[2]):
+                if mentor := Mentor.objects.filter(id=match.group(1)).first():
+                    mentor_fullname = f'{mentor.first_name_en} {mentor.last_name_en}'
+            values[i] = (application_type.label, *value[1:], mentor_fullname)
         return values
     
     def _update_objects(self, instances_data):
