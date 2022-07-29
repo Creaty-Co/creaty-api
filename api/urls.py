b72fd9b7299b1ea7bb4390be0dc1b30a477d8dec
GentleEnvy <komarov.sergei163@gmail.com>
2022-07-29 17:49:04 +0300

    add silk
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -5,24 +5,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.response import Response
-# noinspection PyPackageRequirements
-from silk.profiling.profiler import silk_profile
-
-from base.views import BaseView
 
 mimetypes.add_type('application/javascript', '.js')
-
-
-class CalcView(BaseView):
-    @silk_profile(name='name')
-    def get(self, request):
-        i = 0
-        max_i = int(request.query_params['a'])
-        while i < max_i:
-            i += 1
-        return Response(i)
-
 
 urlpatterns = [
     path('__docs__/', SpectacularAPIView.as_view(), name='__docs__'),
@@ -38,7 +22,6 @@ urlpatterns = [
     path('mailings/', include('mailings.urls')),
     path('pages/', include('pages.urls')),
     path('geo/', include('geo.urls')),
-    path('calc/', CalcView.as_view()),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
