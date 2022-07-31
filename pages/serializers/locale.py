8ec18d508a410a3a0267969f3c3c008711e12163
GentleEnvy <komarov.sergei163@gmail.com>
2022-08-01 01:27:10 +0300

    fix PagesLocaleView
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- pages/serializers/locale.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from pages.models import Locale
-
-
-class PagesLocaleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Locale
-        fields = ['json']
--- pages/views/locale.py
+++ pages/views/locale.py
@@ -1,25 +1,29 @@
 from django.http import Http404
+from rest_framework import serializers
 from rest_framework.mixins import UpdateModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
 
 from admin_.views import BaseAdminView
 from base.utils.decorators import schema_response_204
+from base.utils.functions import schema_serializer
 from base.views import BaseView
 from pages.models import Locale
-from pages.serializers.locale import PagesLocaleSerializer
 
 
 class PagesLocaleView(RetrieveModelMixin, UpdateModelMixin, BaseView):
     queryset = Locale.objects.all()
     lookup_field = 'language'
-    serializer_classes = {'get': PagesLocaleSerializer, 'put': PagesLocaleSerializer}
+    serializer_class = schema_serializer('PagesLocale', key=serializers.JSONField())
     permission_classes_map = {'get': (), 'put': BaseAdminView.permission_classes}
 
     def get(self, request, **_):
-        return self.retrieve(request)
+        return Response(self.get_object().json)
 
     @schema_response_204
     def put(self, request, **_):
-        self.update(request)
+        locale = self.get_object()
+        locale.json = request.data
+        locale.save()
 
     def get_object(self):
         try:
