8b791b6a75a65b9530c45574fbef35656d5a8e97
GentleEnvy <komarov.sergei163@gmail.com>
2023-02-08 18:58:03 +0300

    #118 add google oauth
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UsersMeView, UsersRegisterResendView, UsersRegisterView
+from .views import (
+    UsersMeView,
+    UsersRegisterResendView,
+    UsersRegisterSocialGoogleCompleteView,
+    UsersRegisterSocialGoogleView,
+    UsersRegisterView,
+)
 
 urlpatterns = [
     path('register/', UsersRegisterView.as_view(), name='register'),
     path('register/resend/', UsersRegisterResendView.as_view()),
+    path('register/social/google/', UsersRegisterSocialGoogleView.as_view()),
+    path(
+        'register/social/google/complete/',
+        UsersRegisterSocialGoogleCompleteView.as_view(),
+        name='google_complete',
+    ),
     path('me/', UsersMeView.as_view()),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
