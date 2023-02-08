d0c98a4c4615e346a65fc5cee8dbecaaf2ccb82f
GentleEnvy <komarov.sergei163@gmail.com>
2023-02-08 19:46:47 +0300

    fix cookies
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/users/strategy.py
+++ app/users/strategy.py
@@ -12,22 +12,8 @@ class SocialStrategy(DjangoStrategy):
         if url == settings.SOCIAL_AUTH_LOGIN_REDIRECT_URL:
             token = RefreshToken.for_user(self.request.user)
             response = HttpResponseRedirect(url)
-            response.set_cookie(
-                key='refresh',
-                value=str(token),
-                domain=settings.WEB_DOMAIN,
-                secure=True,
-                httponly=True,
-                samesite='lax',
-            )
-            response.set_cookie(
-                key='access',
-                value=token.access_token,
-                domain=settings.WEB_DOMAIN,
-                secure=True,
-                httponly=True,
-                samesite='lax',
-            )
+            response.set_cookie(key='refresh', value=str(token), httponly=True)
+            response.set_cookie(key='access', value=token.access_token, httponly=True)
             return response
         return super().redirect(url)
 
