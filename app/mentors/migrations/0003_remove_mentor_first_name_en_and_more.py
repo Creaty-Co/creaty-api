73fc88a65aa3d3597c55506618adb88163caa6db
GentleEnvy <komarov.sergei163@gmail.com>
2023-06-02 22:36:06 +0300

    hotfix: last migrations
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/mentors/migrations/0003_remove_mentor_first_name_en_and_more.py
+++ app/mentors/migrations/0003_remove_mentor_first_name_en_and_more.py
@@ -5,14 +5,14 @@ def copy_en_to_mentor_and_mentorinfo_fields(apps, schema_editor):
     Mentor = apps.get_model('mentors', 'Mentor')
     MentorInfo = apps.get_model('mentors', 'MentorInfo')
     for obj in Mentor.objects.all():
-        obj.first_name = obj.first_name_en
-        obj.last_name = obj.last_name_en
-        obj.profession = obj.profession_en
+        obj.first_name = obj.first_name_en or obj.first_name
+        obj.last_name = obj.last_name_en or obj.last_name
+        obj.profession = obj.profession_en or obj.profession
         obj.save(update_fields=['first_name', 'last_name', 'profession'])
     for obj in MentorInfo.objects.all():
-        obj.experience = obj.experience_en
-        obj.resume = obj.resume_en
-        obj.what_help = obj.what_help_en
+        obj.experience = obj.experience_en or obj.experience or ''
+        obj.resume = obj.resume_en or obj.resume
+        obj.what_help = obj.what_help_en or obj.what_help or ''
         obj.save(update_fields=['experience', 'resume', 'what_help'])
 
 
