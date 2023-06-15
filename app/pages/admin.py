253855e23813f5c29bae9128d6e5af2d3c547a06
GentleEnvy <komarov.sergei163@gmail.com>
2023-06-15 17:11:23 +0300

    Changes in the Categories/Tags sections Add a checkbox that would affect the display of the corresponding mentor on the Main page of the Category/Tag
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.contrib import admin
 
+from ..mentors.models import Mentor
 from .models import Page, PageMentors
 
 
@@ -9,8 +11,34 @@ class PageMentorsInline(admin.TabularInline):
     ordering = ['index']
 
 
+class MentorCheckboxWidget(forms.CheckboxSelectMultiple):
+    template_name = 'mentor_checkbox_widget.html'
+
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        option = super().create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+        option['index'] = index
+        return option
+
+
+class PageForm(forms.ModelForm):
+    mentors = forms.ModelMultipleChoiceField(
+        queryset=Mentor.objects.all(),
+        widget=MentorCheckboxWidget,
+        required=False,
+    )
+
+    class Meta:
+        model = Page
+        fields = '__all__'
+
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
+    form = PageForm
     inlines = [PageMentorsInline]
     list_display = ['__str__']
     list_filter = [
--- app/tags/admin.py
+++ app/tags/admin.py
@@ -1,13 +1,76 @@
 from django import forms
 from django.conf import settings
 from django.contrib import admin
+from django.db import transaction
 from django.utils.html import format_html
 
 from app.mentors.models import Mentor
+from app.pages.models import PageMentors
 from app.tags.models import Category, CategoryTag, Tag
 
 
-class CategoryAdminForm(forms.ModelForm):
+class MentorCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
+    template_name = 'mentor_checkbox_widget.html'
+
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        selected = value.index != -1
+        option = super().create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+        option['page_index'] = value.index
+        return option
+
+    def value_from_datadict(self, data, files, name):
+        mentors_map = {mentor_label: mentor for mentor, mentor_label in self.choices}
+        selected_mentor_labels = data.getlist(name)
+        mentors_data = []
+        for selected_mentor_label in selected_mentor_labels:
+            mentor = mentors_map[selected_mentor_label]
+            mentor.index = data.get(f"page_index_{mentor.id}")
+            mentors_data.append(mentor)
+        return mentors_data
+
+
+class MultipleChoiceField(forms.MultipleChoiceField):
+    def to_python(self, value):
+        return value
+
+    def validate(self, value):
+        return value
+
+
+class _BaseAdminForm(forms.ModelForm):
+    mentors_on_page = MultipleChoiceField(
+        widget=MentorCheckboxSelectMultiple(), required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        page = self.instance.page
+        choices = []
+        for mentor in Mentor.objects.all():
+            page_mentors = PageMentors.objects.filter(page=page, mentor=mentor).first()
+            mentor.index = page_mentors.index if page_mentors else -1
+            choices.append((mentor, str(mentor)))
+        self.fields['mentors_on_page'].choices = choices
+
+    @transaction.atomic
+    def save(self, commit=True):
+        mentors = self.cleaned_data['mentors_on_page']
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()
+        page = instance.page
+        if page is not None:
+            page.page_mentors.all().delete()
+            for mentor in mentors:
+                PageMentors.objects.create(page=page, mentor=mentor, index=mentor.index)
+        return instance
+
+
+class CategoryAdminForm(_BaseAdminForm):
     class Meta:
         model = Category
         fields = '__all__'
@@ -24,12 +87,25 @@ class TagInline(admin.TabularInline):
     max_num = 10
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     form = CategoryAdminForm
     list_display = ['title', 'shortcut', 'link']
     readonly_fields = ['link', 'display_icon', 'mentors']
     fieldsets = [
-        (None, {'fields': ['display_icon', 'icon', 'title', 'shortcut', 'link']}),
+        (
+            None,
+            {
+                'fields': [
+                    'display_icon',
+                    'icon',
+                    'title',
+                    'shortcut',
+                    'link',
+                    'mentors_on_page',
+                ]
+            },
+        ),
         ('Mentors', {'fields': ['mentors']}),
     ]
     inlines = [TagInline]
@@ -48,7 +124,14 @@ class CategoryAdmin(admin.ModelAdmin):
         return "No mentors"
 
 
-class TagAdminForm(forms.ModelForm):
+class CategoryInline(admin.TabularInline):
+    verbose_name = "Categories"
+    model = CategoryTag
+    extra = 1
+    max_num = 10
+
+
+class TagAdminForm(_BaseAdminForm):
     class Meta:
         model = Tag
         fields = '__all__'
@@ -58,31 +141,14 @@ class TagAdminForm(forms.ModelForm):
         }
 
 
-class CategoryInline(admin.TabularInline):
-    verbose_name = "Categories"
-    model = CategoryTag
-    extra = 1
-    max_num = 10
-
-
-class MentorInline(admin.TabularInline):
-    verbose_name = "Mentors"
-    model = Mentor.tags.through
-    extra = 1
-    max_num = 10
-
-
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     form = TagAdminForm
     list_display = ['title', 'shortcut', 'link']
-    fields = ['title', 'shortcut', 'link']
-    readonly_fields = ['link']
-    inlines = [CategoryInline, MentorInline]
+    fields = ['title', 'shortcut', 'link', 'mentors_on_page']
+    readonly_fields = ['link', 'page']
+    inlines = [CategoryInline]
 
     def link(self, obj):
         url = f"https://{settings.WEB_DOMAIN}/mentors/{obj.shortcut}"
         return format_html('<a href="{}">{}</a>', url, url)
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Tag, TagAdmin)
