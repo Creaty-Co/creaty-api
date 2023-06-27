b18b4f41234d6e8c1404c653ea79c2d7bbeb0096
GentleEnvy <komarov.sergei163@gmail.com>
2023-06-27 14:41:45 +0300

    hotfix: `PageAdminForm`, `CategoryAdminForm`, `TagAdminForm`
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/tags/admin.py
+++ app/tags/admin.py
@@ -4,6 +4,7 @@ from django import forms
 from django.conf import settings
 from django.contrib import admin
 from django.db import transaction
+from django.db.models import Q
 from django.utils.html import format_html
 
 from app.mentors.models import Mentor
@@ -52,11 +53,11 @@ class _BaseAdminForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         page = self.instance.page
         choices = []
-        mentors = Mentor.objects.all()
+        mentors = Mentor.objects.distinct()
         if page.tag:
-            mentors = mentors.filter(tags=page.tag)
+            mentors = mentors.filter(Q(tags=page.tag) | Q(pages=page))
         elif page.category:
-            mentors = mentors.filter(tags__categories=page.category)
+            mentors = mentors.filter(Q(tags__categories=page.category) | Q(pages=page))
         for mentor in mentors:
             page_mentors = PageMentors.objects.filter(page=page, mentor=mentor).first()
             mentor.index = page_mentors.index if page_mentors else -1
