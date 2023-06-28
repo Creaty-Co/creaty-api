d022dbce25a9647727d058c05362f4ebd371a920
GentleEnvy <komarov.sergei163@gmail.com>
2023-06-28 12:22:16 +0300

    add filters for tags, categories and pages
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/tags/views/general.py
+++ app/tags/views/general.py
@@ -1,5 +1,3 @@
-from django.db.models import Count
-
 from app.admin_.permissions import AdminPermission
 from app.base.views import BaseView
 from app.tags.models import Tag
@@ -10,16 +8,10 @@ class TagsView(BaseView):
     many = True
     permissions_map = {'post': [AdminPermission]}
     serializer_map = {'get': ListTagsSerializer, 'post': POSTTagsSerializer}
-    queryset = Tag.objects.all()
+    queryset = Tag.objects.filter(mentors__isnull=False)
 
     def get(self):
         return self.list()
 
     def post(self):
         return self.create()
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        if AdminPermission().has_permission(self.request, self):
-            return queryset
-        return queryset.annotate(Count('mentor')).exclude(mentor__count=0)
