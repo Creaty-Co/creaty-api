6c7a068fbd3bedea5b61e398e3c7878a71273999
GentleEnvy <komarov.sergei163@gmail.com>
2023-07-03 13:12:20 +0300

    add checks to `check_health` task
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/base/tasks.py
+++ app/base/tasks.py
@@ -1,4 +1,10 @@
+import warnings
+
+import requests
 from celery import shared_task
+from django.conf import settings
+from django.db import connection
+from django_redis import get_redis_connection
 
 # noinspection PyUnresolvedReferences
 from sentry_sdk.crons.decorator import monitor
@@ -7,4 +13,17 @@ from sentry_sdk.crons.decorator import monitor
 @shared_task
 @monitor(monitor_slug='check_health')
 def check_health():
-    pass
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        assert (
+            requests.get(
+                f"https://{settings.API_DOMAIN}/base/status/", verify=False
+            ).json()['status']
+            == "ok"
+        )
+        assert (
+            requests.get(f"https://{settings.WEB_DOMAIN}", verify=False).status_code
+            == 200
+        )
+    assert get_redis_connection().ping()
+    connection.ensure_connection()
