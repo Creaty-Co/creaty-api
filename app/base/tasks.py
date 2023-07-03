3804f297980684c12897868873871e044d58a389
GentleEnvy <komarov.sergei163@gmail.com>
2023-07-03 14:29:42 +0300

    fix checks to `check_health` task
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/base/tasks.py
+++ app/base/tasks.py
@@ -1,8 +1,4 @@
-import warnings
-
-import requests
 from celery import shared_task
-from django.conf import settings
 from django.db import connection
 from django_redis import get_redis_connection
 
@@ -13,17 +9,5 @@ from sentry_sdk.crons.decorator import monitor
 @shared_task
 @monitor(monitor_slug='check_health')
 def check_health():
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        assert (
-            requests.get(
-                f"https://{settings.API_DOMAIN}/base/status/", verify=False
-            ).json()['status']
-            == "ok"
-        )
-        assert (
-            requests.get(f"https://{settings.WEB_DOMAIN}", verify=False).status_code
-            == 200
-        )
     assert get_redis_connection().ping()
     connection.ensure_connection()
