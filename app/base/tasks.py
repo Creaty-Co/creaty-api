57e968fb5c1d000b2314f6ec1663c2d3654d17de
GentleEnvy <komarov.sergei163@gmail.com>
2023-07-07 16:15:32 +0300

    hotfix: add check self in `check_health`
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/base/tasks.py
+++ app/base/tasks.py
@@ -1,3 +1,4 @@
+import requests
 from celery import shared_task
 from django.db import connection
 from django_redis import get_redis_connection
@@ -9,5 +10,6 @@ from sentry_sdk.crons.decorator import monitor
 @shared_task
 @monitor(monitor_slug='check_health')
 def check_health():
+    assert requests.get('http://localhost:8000/base/status/').status_code == 200
     assert get_redis_connection().ping()
     connection.ensure_connection()
