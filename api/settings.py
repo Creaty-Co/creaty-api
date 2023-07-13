dd00c946fd652cdc8537ff92a0b8b508e079a695
GentleEnvy <komarov.sergei163@gmail.com>
2023-07-13 20:22:26 +0300

    hotfix: `_traces_sampler`
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- api/settings.py
+++ api/settings.py
@@ -315,6 +315,8 @@ def _traces_sampler(sampling_context):
         return 0
     if sampling_context.get('asgi_scope', {}).get('path') == '/base/status/':
         return 0
+    if sampling_context.get('wsgi_scope', {}).get('path') == '/base/status/':
+        return 0
     return 1
 
 
