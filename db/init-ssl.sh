5d7c3b2ad5b261610bde589859fb07c8a5679579
GentleEnvy <komarov.sergei163@gmail.com>
2023-07-16 13:18:02 +0300

    add db configs
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- db/init-ssl.sh
+++ db/init-ssl.sh
@@ -8,10 +8,4 @@ chown postgres:postgres /var/lib/postgresql/data/server.key
 chown postgres:postgres /var/lib/postgresql/data/server.crt
 chmod 600 /var/lib/postgresql/data/server.key
 
-{
-  echo "ssl = on"
-  echo "ssl_cert_file = '/var/lib/postgresql/data/server.crt'"
-  echo "ssl_key_file = '/var/lib/postgresql/data/server.key'"
-} >>/var/lib/postgresql/data/postgresql.conf
-
 RUN echo "hostssl all all 0.0.0.0/0 md5" >>/var/lib/postgresql/data/pg_hba.conf
